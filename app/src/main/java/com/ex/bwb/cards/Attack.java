7c6a802eb57d652ee6417c4b46c764b313b61db0
nickv779 <99559784+nickv779@users.noreply.github.com>
2026-03-10 22:39:51 -0400

    setting up proper structures

--- app/src/main/java/com/ex/bwb/cards/Attack.java
+++ app/src/main/java/com/ex/bwb/cards/Attack.java
@@ -3,7 +3,17 @@ package com.ex.bwb.cards;
 import gl.shaders.Texture;
 
 public class Attack extends Card {
-    public Attack(String name, String description, String tagLine, CardType type, Texture artwork) {
+
+    int damage;
+    String target; // Left, Right, Across, All, Any
+    public Attack(String name, String description, String tagLine, CardType type, Texture artwork,
+                  int damage, String target) {
         super(name, description, tagLine, CardType.ATTACK, artwork);
+        this.damage = damage;
+        this.target = target;
+    }
+
+    public void Activate() {
+
     }
 }
